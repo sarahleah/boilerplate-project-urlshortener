13b15573fac70005dcb11fb48fab8558784b8168
Sarah Andreas <79448184+sarahleah@users.noreply.github.com>
2022-11-13 04:44:00 +0000

    little url shortening microservice

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const sites = [];
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,9 +20,43 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/shorturl/:index', function(req, res) {
+	res.redirect(sites[req.params.index]);
 });
+
+function cleanUrl(url) {
+	try {
+		const cleanedUrl = new URL(url);
+		return cleanedUrl.hostname;
+	} catch (error) {
+		return 'susan';
+	}
+}
+
+async function validateUrl(url) {
+	return new Promise((resolve, reject) => {
+		const cleanedUrl = cleanUrl(url)
+		dns.lookup(cleanedUrl, (err, address, family) => {
+			if (err) resolve(false);
+			resolve(true);
+		})
+	})
+}
+
+app.post('/api/shorturl', async function (req, res) {
+	const url = req.body.url;
+	
+	const isValidUrl = await validateUrl(url);
+	if (!isValidUrl) {
+		res.json({ error: 'invalid url' });
+	} else {
+		res.json({
+			original_url: url,
+			short_url: sites.length,
+		})
+		sites.push(url);
+	}
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
